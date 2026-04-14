ffc18f295bad5c27c3b0b1a167c56430b3431fb6
Artin Zarei <artin.zareie@yahoo.com>
2026-04-14 12:34:07 +0330

    Added tests for simdcnn_vecadd_f32_avx2

--- libsimdcnn/include/simdcnn/simdcnn.h
+++ libsimdcnn/include/simdcnn/simdcnn.h
@@ -1,11 +1,17 @@
 #pragma once
 
+#include <stddef.h>
+
 #ifdef __cplusplus
 extern "C"
 {
 #endif
 
     void printhello();
+
+#ifdef HAVE_AVX2
+    extern void simdcnn_vecadd_f32_avx2(float *dst, float *a, float *b, size_t n);
+#endif
 
 #ifdef __cplusplus
 }
--- libsimdcnn/tests/test_vecadd.cpp
+++ libsimdcnn/tests/test_vecadd.cpp
@@ -1,6 +1,26 @@
 #include <gtest/gtest.h>
+#include <simdcnn/simdcnn.h>
 
 TEST(VecAdd, BasicCorrectness)
 {
     ASSERT_EQ(1, 1);
 }
+
+#ifdef HAVE_AVX2
+
+TEST(VecAdd, Avx2Random)
+{
+    const int N = 1024;
+    float *a = (float *)malloc(N * sizeof(float));
+    float *b = (float *)malloc(N * sizeof(float));
+    float *dst = (float *)malloc(N * sizeof(float));
+
+    simdcnn_vecadd_f32_avx2(dst, a, b, N);
+
+    for (size_t i = 0; i < N; i++)
+    {
+        ASSERT_FLOAT_EQ(a[i] + b[i], dst[i]);
+    }
+}
+
+#endif
